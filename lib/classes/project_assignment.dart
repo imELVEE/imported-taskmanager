64074a94440ccb008a307e762269b2d3cc4fde6d
Alvi Aziz <ommarashid@gmail.com>
2024-12-04 15:10:14 -0500

    project class

---
+++ lib/classes/project_assignment.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class ProjectAssignment {
+  int id;
+  DateTime createDate;
+  String subject;
+  String? notes;
+  DateTime? dueDate;
+
+  DateTime? completeDate;
+  bool completed;
+  int members;
+
+  ProjectAssignment({
+    required this.id,
+    required this.createDate,
+    required this.subject,
+    required this.members,
+    this.completed = false,
+    this.notes,
+    this.dueDate,
+    this.completeDate,
+  });
+
+  
+}
