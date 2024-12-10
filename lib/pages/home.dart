6c9d0ed6eafa3a9c176a2605b9c64d3b4759e9c9
Usman Sheikh <us.sheikhh@gmail.com>
2024-12-09 20:48:55 -0500

    added the about section to the home page
    Signed-off-by: Usman Sheikh <us.sheikhh@gmail.com>

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -4,6 +4,10 @@ import 'package:planner_app/pages/support.dart';
 // import 'package:planner_app/pages/calendar.dart';
 import 'package:planner_app/pages/task.dart';
 import 'package:planner_app/pages/project.dart';
+import 'package:url_launcher/link.dart';
+
+
+
 
 class HomePage extends StatefulWidget{
   const HomePage({super.key});
@@ -17,24 +21,39 @@ class HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context){
-    return Scaffold(
+   return Scaffold(
       backgroundColor: Colors.white,
-
       appBar: _topAppBar(),
-
-      body: const Center(
-          child: Text(
-            'Home Page',
-            style: TextStyle(
-              fontSize: 28,
-              fontWeight: FontWeight.bold,
-              color: Color.fromARGB(179, 3, 64, 113),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            AppBar(title: const Text("Group Members:",style: TextStyle(fontSize: 40, color: Colors.black)), centerTitle: true, backgroundColor: Colors.white,),
+            const Column(
+              verticalDirection: VerticalDirection.down,
+              children: [ Text("Usman Sheikh", style: TextStyle(fontSize: 20, color: Colors.black)), Text("Nathan Vasquez",style: TextStyle(fontSize: 20, color: Colors.black)), Text("Xiaomin Chen",style: TextStyle(fontSize: 20, color: Colors.black)), Text("Alvi Aziz",style: TextStyle(fontSize: 20, color: Colors.black))],
             ),
-          )
+            AppBar(title: const Text("Description",style: TextStyle(fontSize: 40, color: Colors.black)), centerTitle: true, backgroundColor: Colors.white, ),
+            const Text("insert description",style: TextStyle(fontSize: 20, color: Colors.black)),
+            AppBar(title: const Text("Code URL:", style: TextStyle(fontSize: 40, color: Colors.black)), centerTitle: true, backgroundColor: Colors.white),
+            Link(uri: Uri.parse('https://gitlab.com/dartapps/hunter/group-project-two-team-six/-/tree/loginpage/lib?ref_type=heads'),
+                target: LinkTarget.blank,
+                builder: (context, followLink) => GestureDetector(
+                  onTap: followLink,
+                  child: const Text(
+                      'Click here to visit Gitlab',
+                      style: TextStyle(color: Colors.blue, decoration: TextDecoration.underline, fontSize: 16),
+                    ),
+                ),
+              ), 
+          ]
+        ),
       ),
+      
+
 
       bottomNavigationBar: _bottomNavBar(),
     );
+
   }
 
   PreferredSizeWidget _topAppBar(){
